10354a5b8a50977bff0fbc349f8f19c256d5436b
Erik Hergins <hergins1@hotmail.com>
2019-04-28 22:05:02 -0500

    functions created

--- liri.js
+++ liri.js
@@ -9,60 +9,69 @@ const spotify = new Spotify(keys.spotify);
 const command = process.argv[2];
 const searchTerm = process.argv.slice(3).join(" ");
 
+function concertThis() {
+    const queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
+    axios
+        .get(queryUrl)
+        .then(function (response) {
+            const returnData = response.data[0];
+            console.log("Event Time & Date: " + moment(returnData.datetime).format('MMMM Do YYYY, h:mm:ss a'));
+            console.log("Venue Name: " + returnData.venue.name);
+            console.log("Venue City: " + returnData.venue.city);
+        })
+
+};
+
+function movieThis() {
+    const movieUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
+
+    axios
+        .get(movieUrl)
+        .then(function (response) {
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Movie Release Year: " + response.data.Year);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1]);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Country Released" + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+};
+
+function spotifyThis() {
+    if (!searchTerm) {
+        searchTerm = "The Sign";
+    }
+        spotify
+            .search({
+                type: "track", query: searchTerm
+            })
+            .then(function (response) {
+                console.log("Song Artist: " + response.tracks.items[0].artists[0].name);
+                console.log("Album Name: " + response.tracks.items[0].album.name);
+                console.log("Song Name: " + response.tracks.items[0].name);
+                console.log("Song Preview: " + response.tracks.items[0].preview_url);
+            })
+};
 
 switch (command) {
     case "concert-this":
-        const queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
-        axios
-            .get(queryUrl)
-            .then(function (response) {
-                const returnData = response.data[0];
-                console.log(moment(returnData.datetime).format('MMMM Do YYYY, h:mm:ss a'));
-                console.log(returnData.venue.name);
-                console.log(returnData.venue.city);
-            })
+        concertThis()
         break;
     case "spotify-this-song":
-        if (!searchTerm) {
-            searchTerm = "The Sign";
-        }
-        if (searchTerm){
-            spotify
-                .search({
-                    type: "track", query: searchTerm
-                })
-                .then(function (response) {
-                    console.log(response.tracks.items[0].artists[0].name);
-                    console.log(response.tracks.items[0].album.name);
-                    console.log(response.tracks.items[0].name);
-                    console.log(response.tracks.items[0].preview_url);
-                })
-            }
+        spotifyThis()
         break;
     case "movie-this":
-        const movieUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
-        
-        axios
-        .get(movieUrl)
-        .then( function(response){
-            console.log(response.data.Title);
-            console.log(response.data.Year);
-            console.log(response.data.Ratings[1]);
-            console.log(response.data.imdbRating);
-            console.log(response.data.Country);
-            console.log(response.data.Language);
-            console.log(response.data.Plot);
-            console.log(response.data.Actors);
-        })
+        movieThis()
         break;
     case "do-what-it-says":
-        fs.readFile("random.txt", "utf8", function(error, data){
-            if(error){
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
                 return console.log(error);
             }
-            
-            const dataArr = data.split(",");
-            console.log(dataArr.join(" "))
+            const dataArr = data.split(",").join(" ");
+            console.log(dataArr);
         })
         break;
     default:
